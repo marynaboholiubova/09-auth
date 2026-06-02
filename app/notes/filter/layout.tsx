95897f9808fe8636f23e19fbfe20b70487af8c62
Maryna Boholiubova <astoret2015@gmail.com>
2026-06-02 20:02:10 +0200

    hw7

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,18 +1,7 @@
-// app/notes/filter/layout.tsx
-
-type Props = {
-  children: React.ReactNode;
-  sidebar: React.ReactNode;
-};
-
-export default function NotesFilterLayout({ children, sidebar }: Props) {
-  return (
-    <>
-      {sidebar}
-      {children}
-    </>
-  );
+export default function NotesFilterPage() {
+  return <div>Notes filter page</div>;
 }
+
 
 
 

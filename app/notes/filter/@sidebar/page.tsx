1fc1ccce40ef124f0b036bbff9fce981bae92e77
Maryna Boholiubova <astoret2015@gmail.com>
2026-06-03 19:23:53 +0200

    homework7update

--- app/notes/filter/@sidebar/page.tsx
+++ app/notes/filter/@sidebar/page.tsx
@@ -1,24 +1,5 @@
-import Link from 'next/link';
-import css from './SidebarNotes.module.css';
+import SidebarNotes from '@/components/SidebarNotes/SidebarNotes';
 
-const tags = ['Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
-
-export default function SidebarNotes() {
-  return (
-    <ul className={css.menuList}>
-      <li className={css.menuItem}>
-        <Link href="/notes/filter/all" className={css.menuLink}>
-          All notes
-        </Link>
-      </li>
-
-      {tags.map(tag => (
-        <li key={tag} className={css.menuItem}>
-          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
-            {tag}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+export default function SidebarPage() {
+  return <SidebarNotes />;
 }
